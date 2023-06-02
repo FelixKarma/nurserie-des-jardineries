f6a62d612a90c4338dc16e5f972f7c0adfa0dfc8
heartycodes <heartycodes@gmail.com>
2023-06-02 16:26:09 +0200

    update on json

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,365 @@
-// class
-//     table of objects
+// Class
+//     Table of objects
 //         innerHTML which contains all modals (and the buttons for the modals)
+// Replace the text/values of the objects with…
 
-    // replace the text/values of the objects with…
+class Potager { 
+    "ID": 1,
+    "LEGUMES": "AIL",
+    "IMAGE": "../",
+    "IMAGE2": "images/Potager-all légumes/01-Ail.png",
+    "IMAGE3": "../images/Potager-all légumes/01-Ail.png",
+    "DÉFINITION": "Alliciée originaire d'Asie, cultivé depuis des siècles et consommé pour ses vertus stimulantes.Plante riche en sels minéraux qui aime les sols légers, sablonneux et peu humide.Exposition ensoleillée",
+    "À ASSOCIER": "carotte, concombre, oignon, fraisier, tomate, épinard, omme de terre, navet",
+    "NE PAS ASSOCIER": "chou, haricot"
+},
+{
+    "ID": 2,
+        "LEGUMES": "ARTICHAUT",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/02-Artichaut.png",
+                    "IMAGE3": "../images/Potager-all légumes/02-Artichaut.png",
+                        "DÉFINITION": "Plante vivace d'origine méditérranéenne qui craint le gel et l'excès d'humidité.Aime les sols limoneux,frais,profonds et bien drainants.",
+                            "À ASSOCIER": "Betterave, Chou, haricot, laitue, poireau, tomate",
+                                "NE PAS ASSOCIER": "Estragon, persil, petit-pois, salsifis"
+},
+{
+    "ID": 3,
+        "LEGUMES": "ASPERGE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/03-Asperge.png",
+                    "IMAGE3": "../images/Potager-all légumes/03-Asperge.png",
+                        "DÉFINITION": "Plante vivace de culture très ancienne originaire d'Europe et d'Asie.L'asperge est très riche en fibres, contient de nombreux sels minéraux, de la vitamine C ainsi que du carotène.Aime les sols légers, sablonneux et drainés.",
+                            "À ASSOCIER": "persil, poireau, tomate",
+                                "NE PAS ASSOCIER": "ail, chou, échalote, oignon, poirée"
+},
+{
+    "ID": 4,
+        "LEGUMES": "AUBERGINE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/05-Aubergine.png",
+                    "IMAGE3": "../images/Potager-all légumes/05-Aubergine.png",
+                        "DÉFINITION": "L'aubergine est un légume très ancien originaire d'Inde. Sa culture nécessite beaucoup de chaleur( minimum 15°) car elle est rès frileuse.Riche en vitamine B et sels minéraux, elle aime les sols profonds, humifères et drainés.Exposition ensoleillée et abritée.",
+                            "À ASSOCIER": "haricot, tomate, pois, piment, thym, persil, estragon",
+                                "NE PAS ASSOCIER": "oignon, pomme de terre"
+},
+{
+    "ID": 5,
+        "LEGUMES": "BETTE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/05-Bette1.png",
+                    "IMAGE3": "../images/Potager-all légumes/05-Bette1.png",
+                        "DÉFINITION": "Plante originaire de Méditérrannée connue depuis l'Antiquité dont la consommation au Moyen-âge fut très importante en Europe. La bette est une très proche cousine de la betterave.Elle aime les sols frais, humifères et drainés.Exposition ensoleillée.",
+                            "À ASSOCIER": "carotte",
+                                "NE PAS ASSOCIER": "betterave, épinard"
+},
+{
+    "ID": 6,
+        "LEGUMES": "BETTERAVE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/06-Betterave.png",
+                    "IMAGE3": "betterave rouge",
+                        "DÉFINITION": "La betterave potagère aussi appelée betterave rouge est originaire d'Europe du sud et fut très consommée par les Romains.Elle se consomme rapée crue en salade ou cuite.Ses feuilles, riches en vitamine A se consomment comme les épinards.elle aime les sols légers, sablonneux et drainés. Exposition ensoleillée.",
+                            "À ASSOCIER": "céleri, chou, laitue, haricot, oignon, radis",
+                                "NE PAS ASSOCIER": "épinard, bette"
+},
+{
+    "ID": 7,
+        "LEGUMES": "BLETTE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/07-Blette.png",
+                    "IMAGE3": "../images/Potager-all légumes/07-Blette.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "carotte, chou-rave, navet, radis, haricot nain",
+                                "NE PAS ASSOCIER": "épinard"
+},
+{
+    "ID": 8,
+        "LEGUMES": "BROCOLI",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/08-Brocoli.png",
+                    "IMAGE3": "../images/Potager-all légumes/08-Brocoli.png",
+                        "DÉFINITION": "Le brocoli est une variété horticole d'un chou d'origine italienne.Le mot brocoli vient de \"brocco\" qui signifie \"pousse\" en italien.De culture très ancienne, les Romains le connaissaient déjà, c'est seulement vers le 16ème siècle qu'il se répand réellement.Riche en vitamien C et sels minéraux.Aime les sols frais et drainés.Exposition mi ombre à ensoleillée.",
+                            "À ASSOCIER": "concombres, pommes de terre, céleri, betteraves, menthe romarin",
+                                "NE PAS ASSOCIER": "fraisiers, radis, tomates"
+},
+{
+    "ID": 9,
+        "LEGUMES": "CAROTTE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/09-Carotte.png",
+                    "IMAGE3": "../images/Potager-all légumes/09-Carotte.png",
+                        "DÉFINITION": "La carotte est principalement originaire du bassin méditérranéen.Cultivées depuis des siècles les carottes étaient à l'origine blanches, jaunes ou violettes.La carotte renferme une grande quantité de vitamines, sels minéraux, d'oligoélémnets ainsi que des caroténoides.Aime les sols légers, humifères, sablonneux et drainés.Exposition mi ombre à ensoleillée.",
+                            "À ASSOCIER": "ail, aneth, endive haricot, laitue, oignon, petit pois, oireau radis salade tomate ciboulette",
+                                "NE PAS ASSOCIER": "betterave, menthe"
+},
+{
+    "ID": 10,
+        "LEGUMES": "CÉLERI",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/10-Céleri.png",
+                    "IMAGE3": "../images/Potager-all légumes/10-Céleri.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "choux, épinard, haricot, poireau, pois, tomate",
+                                "NE PAS ASSOCIER": "salade, pomme de terre"
+},
+{
+    "ID": 11,
+        "LEGUMES": "CHOU",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/11-Chou.png",
+                    "IMAGE3": "../images/Potager-all légumes/11-Chou.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "betterave, haricot, laitue, petit pois, salade, tomate",
+                                "NE PAS ASSOCIER": "autres variétés de chou, ail"
+},
+{
+    "ID": 12,
+        "LEGUMES": "CHOU-FLEUR",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/12-Chou-fleur.png",
+                    "IMAGE3": "../images/Potager-all légumes/12-Chou-fleur.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "céleri, tomate, oignon, salade",
+                                "NE PAS ASSOCIER": "chou"
+},
+{
+    "ID": 13,
+        "LEGUMES": "CONCOMBRE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/13-Concombre.png",
+                    "IMAGE3": "../images/Potager-all légumes/13-Concombre.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "aneth, chou, haricot, laitue, oignon",
+                                "NE PAS ASSOCIER": "radis"
+},
+{
+    "ID": 14,
+        "LEGUMES": "COURGE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/14-Courge.png",
+                    "IMAGE3": "../images/Potager-all légumes/14-Courge.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "haricot, laitue, maïs, navet",
+                                "NE PAS ASSOCIER": "pois"
+},
+{
+    "ID": 15,
+        "LEGUMES": "COURGETTE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/15 Courgette.png",
+                    "IMAGE3": "../images/Potager-all légumes/15 Courgette.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "ciboulette, échalote, haricot, oignon, petit pois",
+                                "NE PAS ASSOCIER": "concombre"
+},
+{
+    "ID": 16,
+        "LEGUMES": "CORNICHON",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/16-Cornichon.png",
+                    "IMAGE3": "../images/Potager-all légumes/16-Cornichon.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "basilic, chou, haricot, laitue, œillet d'Inde, oignon, pois",
+                                "NE PAS ASSOCIER": "melon, pomme de terre, tomate"
+},
+{
+    "ID": 17,
+        "LEGUMES": "ENDIVE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/17-Endive.png",
+                    "IMAGE3": "../images/Potager-all légumes/17-Endive.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "Carotte, navet, poireau, radis",
+                                "NE PAS ASSOCIER": "artichaut, estragon, laitue, salsifis, topinambour"
+},
+{
+    "ID": 18,
+        "LEGUMES": "ÉPINARD",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/18-épinard.png",
+                    "IMAGE3": "../images/Potager-all légumes/18-épinard.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "haricot, petit pois, fraisier, chou, radis, salade, tomate",
+                                "NE PAS ASSOCIER": "betterave rouge"
+},
+{
+    "ID": 19,
+        "LEGUMES": "FENOUIL",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/19-Fenouil.png",
+                    "IMAGE3": "../images/Potager-all légumes/19-Fenouil.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "céleri, navet, poireau",
+                                "NE PAS ASSOCIER": "chou, tomate"
+},
+{
+    "ID": 20,
+        "LEGUMES": "FÈVE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/20-Fève.png",
+                    "IMAGE3": "../images/Potager-all légumes/20-Fève.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "tomate, fraise, courge",
+                                "NE PAS ASSOCIER": "ail, ciboulette"
+},
+{
+    "ID": 21,
+        "LEGUMES": "FRAISIER",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/21-Fraisier.png",
+                    "IMAGE3": "../images/Potager-all légumes/21-Fraisier.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "ail, laitue, oignon, poireau, échalote, persil, sauge",
+                                "NE PAS ASSOCIER": "chou"
+},
+{
+    "ID": 22,
+        "LEGUMES": "HARICOT",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/22-Haricot.png",
+                    "IMAGE3": "../images/Potager-all légumes/22-Haricot.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "carotte, chou, fraisier, laitue, aubergine, céleri, courgette, poireau, radis, pomme de terre",
+                                "NE PAS ASSOCIER": "ail, oignon, pois, poireau"
+},
+{
+    "ID": 23,
+        "LEGUMES": "LAITUE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/23-Laitue.png",
+                    "IMAGE3": "../images/Potager-all légumes/23-Laitue.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "betterave, carotte, cerfeuil, chou, concombre/cornichon, fraisier haricot radis",
+                                "NE PAS ASSOCIER": "tournesol"
+},
+{
+    "ID": 24,
+        "LEGUMES": "MÂCHE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/24-Mâche.png",
+                    "IMAGE3": "../images/Potager-all légumes/24-Mâche.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "ail, basilic, haricot, pois, radis",
+                                "NE PAS ASSOCIER": "chou, épinard, salade"
+},
+{
+    "ID": 25,
+        "LEGUMES": "MAIS",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/25-Maïs.png",
+                    "IMAGE3": "../images/Potager-all légumes/25-Maïs.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "concombre, courge, courgette",
+                                "NE PAS ASSOCIER": "betterave"
+},
+{
+    "ID": 26,
+        "LEGUMES": "MELON",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/26-Melon.png",
+                    "IMAGE3": "../images/Potager-all légumes/26-Melon.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "maïs, tournesol, potiron",
+                                "NE PAS ASSOCIER": "concombre"
+},
+{
+    "ID": 27,
+        "LEGUMES": "NAVET",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/27-Navet.png",
+                    "IMAGE3": "../images/Potager-all légumes/27-Navet.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "ail, céleri, ciboulette, épinard, petit pois",
+                                "NE PAS ASSOCIER": "radis"
+},
+{
+    "ID": 28,
+        "LEGUMES": "OIGNON",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/28-Oignon.png",
+                    "IMAGE3": "../images/Potager-all légumes/28-Oignon.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "aneth, ail, carotte, concombre, salade, tomate",
+                                "NE PAS ASSOCIER": "haricot, petit pois, poireau, chou, fève"
+},
+{
+    "ID": 29,
+        "LEGUMES": "PANAIS",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/29-Panais.png",
+                    "IMAGE3": "../images/Potager-all légumes/29-Panais.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "Ail, laitue, navet, oignon, petit-pois, radis, salsifis, tomate",
+                                "NE PAS ASSOCIER": "Aneth, bette, carotte, céleri, fenouil, persil, pomme de terre"
+},
+{
+    "ID": 30,
+        "LEGUMES": "PETIT-POIS",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/30-petit-pois.png",
+                    "IMAGE3": "../images/Potager-all légumes/30-petit-pois.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "carotte, épinard, radis, chou, mâche, pomme de terre",
+                                "NE PAS ASSOCIER": "ail, échalote, oignon, tomate"
+},
+{
+    "ID": 31,
+        "LEGUMES": "PIMENT",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/31-Piment.png",
+                    "IMAGE3": "../images/Potager-all légumes/31-Piment.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "basilic, carotte, oignon",
+                                "NE PAS ASSOCIER": "fenouil"
+},
+{
+    "ID": 32,
+        "LEGUMES": "POIREAU",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/32-Poireau.png",
+                    "IMAGE3": "../images/Potager-all légumes/32-Poireau.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "carotte, fraisier, épinard, tomate",
+                                "NE PAS ASSOCIER": "haricot, pois"
+},
+{
+    "ID": 33,
+        "LEGUMES": "POMME DE TERRE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/33-pomme de terre.png",
+                    "IMAGE3": "../images/Potager-all légumes/33-pomme de terre.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "ail, haricot, pois",
+                                "NE PAS ASSOCIER": "aubergine, courgette, radis, tomate"
+},
+{
+    "ID": 34,
+        "LEGUMES": "RADIS",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/34-Radis.png",
+                    "IMAGE3": "../images/Potager-all légumes/34-Radis.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "carotte, fraisier, haricot, salade, tomate, cresson",
+                                "NE PAS ASSOCIER": "cerfeuil, concombre"
+},
+{
+    "ID": 35,
+        "LEGUMES": "SALADE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/35-Salade.png",
+                    "IMAGE3": "../images/Potager-all légumes/35-Salade.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "aneth, betterave, concombre, chou, épinard, haricot, etit ois, radis, tomate",
+                                "NE PAS ASSOCIER": "persil, tournesol"
+},
+{
+    "ID": 36,
+        "LEGUMES": "TOMATE",
+            "IMAGE": "../",
+                "IMAGE2": "images/Potager-all légumes/36-tomates.png",
+                    "IMAGE3": "../images/Potager-all légumes/36-tomates.png",
+                        "DÉFINITION": "",
+                            "À ASSOCIER": "carotte, chou, épinard, oignon, persil, poireau, salade, basilic",
+                                "NE PAS ASSOCIER": "betterave, chou rouge, fenouil, pois"
+}
